424f1b6abc440d3d21ea6f08414b1708312dde4d
Andrei Lapets <a@lapets.io>
2018-01-13 23:56:06 -0500

    Adjust version number.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name             = 'primes',
+    version          = '0.1.0.0',
     packages         = ['primes',],
     install_requires = ['isqrt',],
     license          = 'MIT',
